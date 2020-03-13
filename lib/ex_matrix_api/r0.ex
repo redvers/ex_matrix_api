b92639e9e2e0b5ea1a549cc69856b0731831fbf0
Redvers Davies <red@infect.me>
2020-03-13 01:16:56 +0000

    Converted to FSM

--- lib/ex_matrix_api/r0.ex
+++ lib/ex_matrix_api/r0.ex
@@ -1,15 +1,15 @@
 defmodule ExMatrixApi.R0 do
 
-  def connect(ref), do: GenServer.call(ref, :connect)
-  def versions(ref), do: GenServer.call(ref, :r0_versions)
-  def login_post(ref), do: GenServer.call(ref, :login)
-  def logout(ref), do: GenServer.call(ref, :r0_logout)
-  def whoami(ref), do: GenServer.call(ref, :r0_whoami)
-  def capabilities(ref), do: GenServer.call(ref, :r0_capabilities)
-  def joined_rooms(ref), do: GenServer.call(ref, :r0_joined_rooms)
-  def sync(ref, since), do: GenServer.call(ref, {:r0_sync, since})
-  def sync(ref), do: GenServer.call(ref, :r0_sync)
-  def send(ref, roomid, eventtype, txnID, content), do: GenServer.call(ref, {:r0_send_event, roomid, eventtype, txnID, content})
+  def connect(ref),       do: GenStateMachine.call(ref, :connect)
+  def versions(ref),      do: GenStateMachine.call(ref, :r0_versions)
+  def login_post(ref),    do: GenStateMachine.call(ref, :login)
+  def logout(ref),        do: GenStateMachine.call(ref, :r0_logout)
+  def whoami(ref),        do: GenStateMachine.call(ref, :r0_whoami)
+  def capabilities(ref),  do: GenStateMachine.call(ref, :r0_capabilities)
+  def joined_rooms(ref),  do: GenStateMachine.call(ref, :r0_joined_rooms)
+  def sync(ref, since),   do: GenStateMachine.call(ref, {:r0_sync, since})
+  def sync(ref),          do: GenStateMachine.call(ref, :r0_sync)
+#  def send(ref, roomid, eventtype, txnID, content), do: GenServer.call(ref, {:r0_send_event, roomid, eventtype, txnID, content})
 
 
 
--- lib/ex_matrix_api/worker.ex
+++ lib/ex_matrix_api/worker.ex
@@ -1,91 +1,66 @@
 require Logger
 defmodule ExMatrixApi.Worker do
-  defstruct [:id, :homeserver, :port, :sessionid, :deviceid, :username, :password, :userstate, :gunpid]
+  defstruct [:id, :homeserver, :port, :sessionid, :deviceid, :username, :password, :gunpid]
 
-  use GenServer
-
+  use GenStateMachine
+ 
   def start_link(state = %ExMatrixApi.Worker{id: id}) when is_atom(id) do
-    GenServer.start_link(__MODULE__, state, [])
+    {:ok, pid} = GenStateMachine.start_link(__MODULE__, {:init, state})
+    Process.register(pid, state.id)
+
+    {:ok, pid}
   end
 
-  def init(state) do
-    Process.register(self(), state.id)
-    Logger.debug(inspect(state.id))
-    {:ok, state}
+ 
+  def handle_event({:call, from}, :connect,         :init,          state), do: connect_action({:call, from}, :connect, :init, state)
+  def handle_event({:call, from}, :r0_versions,     :connected,     state), do: version_action(state, from)
+  def handle_event({:call, from}, :login,           :connected,     state), do:   login_action({:call, from}, :login, :connected, state)
+
+  def handle_event({:call, from}, :r0_logout,       :authenticated, state), do: logout_r0_action(from, state)
+  def handle_event({:call, from}, :r0_whoami,       :authenticated, state), do: whoami_r0_action(from, state)
+  def handle_event({:call, from}, :r0_capabilities, :authenticated, state), do: capabi_r0_action(from, state)
+  def handle_event({:call, from}, :r0_joined_rooms, :authenticated, state), do: join_r_r0_action(from, state)
+  def handle_event({:call, from}, :r0_sync,         :authenticated, state), do: sync_r0_action(from, state)
+  def handle_event({:call, from}, {:r0_sync, t},    :authenticated, state), do: sync_r0_action(from, t, state)
+
+  def handle_event(:info, {:gun_up,_,_},         fsm, state), do: {:next_state, fsm, state}
+  def handle_event(:info, {:gun_down,_,_},       fsm, state), do: {:next_state, fsm, state}
+  def handle_event(:info, {:gun_up,_,_,_,_,_},   fsm, state), do: {:next_state, fsm, state}
+  def handle_event(:info, {:gun_down,_,_,_,_,_}, fsm, state), do: {:next_state, fsm, state}
+
+  def handle_event(:info, {:gun_response,_pid,_ref, _, _httpcode, headers}, fsm, state) do
+    inspect(headers) |> Logger.info
+    {:next_state, fsm, state}
   end
 
 
-##############################################################################
-#connect - requires host & port
-##############################################################################
-
-
-  def get_noauth(state, url) do
-    ref = :gun.get(state.gunpid, url)
-    :gun.await_body(state.gunpid, ref)
+  def handle_event(foo, state) do
+    Logger.debug(inspect(foo)) 
+    {:noreply, state}
   end
 
-    
-
-  def handle_call(:r0_versions, _from, state), do: {:reply, get_noauth(state, '/_matrix/client/versions'), state}
 
 
 
-  def post_authed(state, url, body) do
-    ref = :gun.post(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"}], body)
-    {:ok, json} = :gun.await_body(state.gunpid, ref)
-    Poison.decode!(json)
-  end
-  def get_authed(state, url) do
-    ref = :gun.get(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"}])
-    {:ok, json} = :gun.await_body(state.gunpid, ref)
-    Poison.decode!(json)
-  end
-  def put_authed(state, url, body) do
-    ref = :gun.put(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"},{"Content-type", "application/json"}], body)
-    {:ok, json} = :gun.await_body(state.gunpid, ref)
-    Poison.decode!(json)
+
+
+  def sync_r0_action(from, state), do: {:next_state, :authenticated, state, [{:reply, from, get_authed(state, "/_matrix/client/r0/sync")}]}
+  def sync_r0_action(from, t, state), do: {:next_state, :authenticated, state, [{:reply, from, get_authed(state, "/_matrix/client/r0/sync?since=#{t}")}]}
+  def join_r_r0_action(from, state), do: {:next_state, :authenticated, state, [{:reply, from, get_authed(state, '/_matrix/client/r0/joined_rooms')}]}
+  def capabi_r0_action(from, state), do: {:next_state, :authenticated, state, [{:reply, from, get_authed(state, '/_matrix/client/r0/capabilities')}]}
+
+  def logout_r0_action(from, state) do
+    newstate =
+      state
+      |> Map.put(:deviceid, nil)
+      |> Map.put(:sessionid, nil)
+    {:next_state, :connected, newstate, [{:reply, from, post_authed(state, '/_matrix/client/r0/logout', "")}]}
   end
 
-  def handle_call({:r0_send_event, roomid, eventtype, txnID, content}, _from, state), do: {:reply, put_authed(state, "/_matrix/client/r0/rooms/#{roomid}/send/#{eventtype}/#{txnID}", content), state}
+  def whoami_r0_action(from, state), do: {:next_state, :authenticated, state, [{:reply, from, get_authed(state, '/_matrix/client/r0/account/whoami')}]}
 
-
-  def handle_call(:r0_logout, _from, state), do: {:reply, post_authed(state, '/_matrix/client/r0/logout', ""), state}
-
-  def handle_call(:r0_whoami, _from, state), do: {:reply, get_authed(state, '/_matrix/client/r0/account/whoami'), state}
-
-  def handle_call(:r0_capabilities, _from, state), do: {:reply, get_authed(state, '/_matrix/client/r0/capabilities'), state}
-
-  def handle_call(:r0_joined_rooms, _from, state), do: {:reply, get_authed(state, '/_matrix/client/r0/joined_rooms'), state}
-
-  def handle_call(:r0_sync, _from, state), do: {:reply, get_authed(state, "/_matrix/client/r0/sync"), state}
-  def handle_call({:r0_sync, since}, _from, state), do: {:reply, get_authed(state, "/_matrix/client/r0/sync?since=#{since}"), state}
-
-##############################################################################
-# Special Cases
-##############################################################################
-
-  def handle_call(:connect, _from, state =
-    %ExMatrixApi.Worker{userstate: nil,
-                        gunpid: nil,
-                        homeserver: hs, port: port}) do
-
-   homeserver = String.to_charlist(hs)
-
-   {:ok, pid} = :gun.open(homeserver, port, %{transport: :tls})
-   case :gun.await_up(pid) do
-     {:error, error} -> Logger.debug(inspect(error))
-                        :gun.close(pid)
-                        {:reply, {:error, error}, state}
-     {:ok, :http}    -> Logger.debug("Connected...")
-                        newstate = state
-                                   |> Map.put(:gunpid, pid)
-                                   |> Map.put(:userstate, :connected)
-                        {:reply, :ok, newstate}
-   end
-  end
-  def handle_call(:login, _from, state = 
-      %ExMatrixApi.Worker{userstate: :connected, gunpid: gunpid}) do
+  def version_action(state, from), do: {:next_state, :connected, state, [{:reply, from, get_noauth(state, '/_matrix/client/versions')}]}
+  def login_action({:call, from}, :login, :connected, state = %ExMatrixApi.Worker{gunpid: gunpid}) do
       body = 
       %{identifier: %{type: "m.id.user", user: state.username},
         password: state.password,
@@ -96,39 +71,56 @@ defmodule ExMatrixApi.Worker do
     {:ok, json} = :gun.await_body(gunpid, ref)
     res = Poison.decode!(json)
 
-    newstate = state
-               |> Map.put(:userstate, :authenticated)
+    case Map.get(res, "access_token", :fail) do
+      :fail -> {:next_state, :connected, state, [{:reply, from, res}]}
+          _ -> newstate = state
                |> Map.put(:username, Map.get(res, "user_id"))
                |> Map.put(:deviceid, Map.get(res, "device_id"))
                |> Map.put(:sessionid, Map.get(res, "access_token"))
 
-    {:reply, res, newstate}
+               {:next_state, :authenticated, newstate, [{:reply, from, res}]}
+    end
 
   end
 
+  def connect_action({:call, from}, :connect, :init, state = %ExMatrixApi.Worker{gunpid: nil, homeserver: hs, port: port}) do
+   homeserver = String.to_charlist(hs)
 
-
-
-
-  defp check_connection(state = %ExMatrixApi.Worker{gunpid: pid}) when is_pid(pid), do: {:ok, state}
-  defp check_connection(_), do: {:error, :noconnection}
-
-
-  def handle_info({:gun_up, _, _}, state), do: {:noreply, state}
-  def handle_info({:gun_down, _, _}, state), do: {:noreply, state}
-  def handle_info({:gun_up, _, _,_,_,_}, state), do: {:noreply, state}
-  def handle_info({:gun_down, _, _,_,_,_}, state), do: {:noreply, state}
-
-  def handle_info({:gun_response,_pid,_ref, _, 200, headers}, state) do
-    inspect(headers) |> Logger.info
-    {:noreply, state}
+   {:ok, pid} = :gun.open(homeserver, port, %{transport: :tls})
+   case :gun.await_up(pid) do
+     {:error, error} -> Logger.debug(inspect(error))
+                        :gun.close(pid)
+                        {:next_state, :init, state, [{:reply, from, {:error, error}}]}
+     {:ok, :http}    -> Logger.debug("Connected...")
+                        newstate = state
+                                   |> Map.put(:gunpid, pid)
+                        {:next_state, :connected, newstate, [{:reply, from, :ok}]}
+    end
   end
 
-
-  def handle_info(foo, state) do
-    Logger.debug(inspect(foo)) 
-    {:noreply, state}
+  def get_noauth(state, url) do
+    ref = :gun.get(state.gunpid, url)
+    :gun.await_body(state.gunpid, ref)
   end
+ 
+  def post_authed(state, url, body) do
+    ref = :gun.post(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"}], body)
+    {:ok, json} = :gun.await_body(state.gunpid, ref)
+    Poison.decode!(json)
+  end
+
+  def get_authed(state, url) do
+    ref = :gun.get(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"}])
+    {:ok, json} = :gun.await_body(state.gunpid, ref)
+    Poison.decode!(json)
+  end
+#   def put_authed(state, url, body) do
+#     ref = :gun.put(state.gunpid, url, [{"Authorization", "Bearer #{state.sessionid}"},{"Content-type", "application/json"}], body)
+#     {:ok, json} = :gun.await_body(state.gunpid, ref)
+#     Poison.decode!(json)
+#   end
+# 
+#   def handle_call({:r0_send_event, roomid, eventtype, txnID, content}, _from, state), do: {:reply, put_authed(state, "/_matrix/client/r0/rooms/#{roomid}/send/#{eventtype}/#{txnID}", content), state}
 
 
 end
